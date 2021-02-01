430dc5ce2521a8e11f9128e37a325b82f8f21b63
GintarasRadzevicius <g.radzevicius@gmail.com>
2021-02-01 17:14:44 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -118,13 +118,12 @@ function doSomething() {
   } else if(startY < endY){
     rotateCubeDown()
   }
-
 }
 
 function processTouchStart(ev){
 
   requestAnimationFrame(timer);
-  // ev.preventDefault();
+  ev.preventDefault();
 
   identifyWhichCube(ev);
 
@@ -134,15 +133,15 @@ function processTouchStart(ev){
   date = new Date();
   time = date.getTime();
   fingerPressTime = time;
+
 }
 
-function processTouchEnd(){
+function processTouchEnd(ev){
 
   cancelAnimationFrame(timerID);
   counter = 0;
 
-
-  // ev.preventDefault();
+  ev.preventDefault();
 
   if(timeNew - fingerPressTime >= 500){return;}
 
@@ -157,9 +156,14 @@ function processTouchEnd(){
   }
 }
 
-function processTouchMove(ev){endY = ev.changedTouches[0].pageY;}
+function processTouchMove(ev){
 
-function identifyWhichCube(ev){ //sita vieta taisyti ????
+  ev.preventDefault();
+  endY = ev.changedTouches[0].pageY;
+
+}
+
+function identifyWhichCube(ev){
   targetElement = ev.targetTouches[0].target;
 
   switch (targetElement.className) {
@@ -219,7 +223,7 @@ switch (cubeNo) {
   }
 
   changeNumbersUp(cubeNo);
-
+  // calculateResult();
 
 }
 
@@ -243,7 +247,7 @@ function rotateCubeDown(){
   }
 
   changeNumbersDown(cubeNo);
-
+  // calculateResult();
 }
 
 function findFrontAfterChange(cube, rotation){
@@ -270,148 +274,148 @@ var day;
 var month;
 var year;
 
-function changeNumbersUp(cubeNumber){ //nebaigta!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+var dayFrom;
+var monthFrom;
+var yearFrom;
+var dayTo;
+var monthTo;
+var yearTo;
 
-  var activeFrontClassDay;
-  var activeFrontClassMonth;
-  var activeFrontClassYear;
-  var cubeDayId;
-  var cubeMonthId;
-  var cubeYearId;
 
-  switch(cubeNumber){
-    case 0:
-    case 3:
-      activeFrontClassDay = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
-      activeFrontClassMonth = arrCubeSidesClasses[arrSideCounter[cubeNumber + 1]];
-      activeFrontClassYear = arrCubeSidesClasses[arrSideCounter[cubeNumber + 2]];
-      
-      cubeDayId = 'cube' + (cubeNumber + 1);
-      cubeMonthId = 'cube' + (cubeNumber + 2);
-      cubeYearId = 'cube' + (cubeNumber + 3);
-      
+function changeNumbersUp(cubeNumber){
+
+  let activeFrontClass = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
+  let cubeId = 'cube' + (cubeNumber + 1);
+
+  findFrontAfterChange(cubeNumber, 'up');
+
+  activeFrontClass = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
+  document.getElementById(cubeId).getElementsByClassName(activeFrontClass)[0].innerText = updateDateUp(cubeNumber);
   
-      findFrontAfterChange(cubeNumber, 'up');
+  changeDayAccordingToMonth(cubeNumber); //changing day when month and year changes
 
-      activeFrontClassDay = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
-
-      if(day == 1){
-        maxDays = getDaysfromMonthAndYear(month, year);
-        day = maxDays;
-      } else{
-        day--;
-      }
-
-      document.getElementById(cubeDayId).getElementsByClassName(activeFrontClassDay)[0].innerText = day;
-
-      break;
-
-
-    case 1:
-    case 4:
-      activeFrontClassMonth = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
-      cubeMonthId = 'cube' + (cubeNumber + 1);
-
-      findFrontAfterChange(cubeNumber, 'up');
-
-      activeFrontClassMonth = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
-
-      if(month == 1){
-        month = 12;
-      } else{
-        month--;
-      }
-      document.getElementById(cubeMonthId).getElementsByClassName(activeFrontClassMonth)[0].innerText = month;
-
-      break;
-
-
-    case 2:
-    case 5:
-      activeFrontClassYear = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
-      cubeYearId = 'cube' + (cubeNumber + 1);
-
-      findFrontAfterChange(cubeNumber, 'up');
-
-      activeFrontClassYear = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
-      
-      year--;
-
-      document.getElementById(cubeYearId).getElementsByClassName(activeFrontClassYear)[0].innerText = year;
-
-      break;
-    }
 }
 
 function changeNumbersDown(cubeNumber){
 
-  var activeFrontClassDay;
-  var activeFrontClassMonth;
-  var activeFrontClassYear;
-  var cubeDayId;
-  var cubeMonthId;
-  var cubeYearId;
+  let activeFrontClass;
+  let cubeId = 'cube' + (cubeNumber + 1);
 
-  switch(cubeNumber){
-    case 0:
-    case 3:
-      activeFrontClassDay = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
-      activeFrontClassMonth = arrCubeSidesClasses[arrSideCounter[cubeNumber + 1]];
-      activeFrontClassYear = arrCubeSidesClasses[arrSideCounter[cubeNumber + 2]];
-      
-      cubeDayId = 'cube' + (cubeNumber + 1);
-      cubeMonthId = 'cube' + (cubeNumber + 2);
-      cubeYearId = 'cube' + (cubeNumber + 3);
-      
-      maxDays = getDaysfromMonthAndYear(month, year);
-  
-      findFrontAfterChange(cubeNumber, 'down');
+  findFrontAfterChange(cubeNumber, 'down');
 
-      activeFrontClassDay = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
+  activeFrontClass = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
+  document.getElementById(cubeId).getElementsByClassName(activeFrontClass)[0].innerText = updateDateDown(cubeNumber);
 
-      if(day == maxDays){
-        day = 1;
-      } else{
-        day = 1 + day;
-      }
-        document.getElementById(cubeDayId).getElementsByClassName(activeFrontClassDay)[0].innerText = day;
+  changeDayAccordingToMonth(cubeNumber);
 
-      break;
+}
 
-
+function changeDayAccordingToMonth(cubeNumber) {
+  switch (cubeNumber) {   
     case 1:
-    case 4:
-      activeFrontClassMonth = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
-      cubeMonthId = 'cube' + (cubeNumber + 1);
-
-      findFrontAfterChange(cubeNumber, 'down');
-
-      activeFrontClassMonth = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
-
-      if(month == 12){
-        month = 1;
-      } else{
-        month++;
-      }
-      document.getElementById(cubeMonthId).getElementsByClassName(activeFrontClassMonth)[0].innerText = month;
-
-      break;
-
-
     case 2:
-    case 5:
-      activeFrontClassYear = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
-      cubeYearId = 'cube' + (cubeNumber + 1);
-
-      findFrontAfterChange(cubeNumber, 'down');
-
-      activeFrontClassYear = arrCubeSidesClasses[arrSideCounter[cubeNumber]];
-
-      year++;
-      document.getElementById(cubeYearId).getElementsByClassName(activeFrontClassYear)[0].innerText = year;
-
+      if (monthFrom == 2 && dayFrom > 27) {
+        let maxDays = getDaysfromMonthAndYear(monthFrom, yearFrom);
+        if (dayFrom > maxDays) {
+          document.getElementById('cube1').getElementsByClassName(arrCubeSidesClasses[arrSideCounter[0]])[0].innerText = maxDays;
+          dayFrom = maxDays;
+        }
+      }
       break;
-    }
+
+    case 4:
+    case 5:
+      if (monthTo == 2 && dayTo > 27) {
+        let maxDays = getDaysfromMonthAndYear(monthTo, yearTo);
+        if (dayTo > maxDays) {
+          document.getElementById('cube4').getElementsByClassName(arrCubeSidesClasses[arrSideCounter[3]])[0].innerText = maxDays;
+          dayTo = maxDays;
+        }
+      }
+      break;
+  }
+
+}
+
+function updateDateUp(cubeNumber){
+  switch (cubeNumber) {
+
+    case 0:
+      if(dayFrom == 1){dayFrom = getDaysfromMonthAndYear(monthFrom, yearFrom);} else{dayFrom--;}
+      return dayFrom;
+      break;
+  
+    case 3:
+      if(dayTo == 1){dayTo = getDaysfromMonthAndYear(monthTo, yearTo);} else{dayTo--;}
+      return dayTo;
+      break;
+  
+    case 1:
+      if(monthFrom == 1){monthFrom = 12;} else{monthFrom--;}
+      return monthFrom;
+      break;
+    
+    case 4:
+      if(monthTo == 1){monthTo = 12;} else{monthTo--;}
+      return monthTo;
+      break;
+      
+    case 2:
+      yearFrom--;
+      return yearFrom;
+      break;
+  
+    case 5:
+      yearTo--;
+      return yearTo;
+      break;
+    
+                                          
+  }
+}
+
+function updateDateDown(cubeNumber){
+
+  switch (cubeNumber) {
+
+    case 0:
+      if (dayFrom > 27) {
+        let maxDays = getDaysfromMonthAndYear(monthFrom, yearFrom);
+        if(dayFrom == maxDays){dayFrom = 1;} else{dayFrom++;}
+      }
+      return dayFrom;
+      break;
+  
+    case 3:
+      if (dayTo > 27) {
+        let maxDays = getDaysfromMonthAndYear(monthFrom, yearFrom);
+        if(dayTo == maxDays){dayTo = 1;} else{dayTo++;}
+      }
+      return dayTo;
+      break;
+  
+    case 1:
+      if(monthTo == 12){monthTo = 1;} else{monthTo++;}
+      return monthTo;
+      break;
+    
+    case 4:
+      if(monthTo == 12){monthTo = 1;} else{monthTo++;}
+      return monthTo;
+      break;
+      
+    case 2:
+      yearFrom++;
+      return yearFrom;
+      break;
+  
+    case 5:
+      yearTo++;
+      return yearTo;
+      break;
+    
+                                          
+  }
 }
 
 function getDomElements(){
@@ -446,35 +450,44 @@ function getDomElements(){
   arrCube_6_ElRef[3] = document.getElementById('cube6').getElementsByClassName('cubeSideBottomText')[0];
 }
 
+var dayFrom;
+var monthFrom;
+var yearFrom;
+var dayTo;
+var monthTo;
+var yearTo;
+
+
+
 function firstFillCubeNumbers(){
   
   getDomElements();
 
-  day = new Date().getDate();
-  month = (new Date().getMonth()) + 1;
-  year = new Date().getFullYear();
+  dayFrom = dayTo = new Date().getDate();
+  monthFrom = monthTo = (new Date().getMonth()) + 1;
+  yearFrom = yearTo = new Date().getFullYear();
 
-  arrCube_1_ElRef[0].innerText = arrCube_4_ElRef[0].innerText = day;
-  arrCube_2_ElRef[0].innerText = arrCube_5_ElRef[0].innerText = month;
-  arrCube_3_ElRef[0].innerText = arrCube_6_ElRef[0].innerText = year;
+  arrCube_1_ElRef[0].innerText = arrCube_4_ElRef[0].innerText = dayFrom;
+  arrCube_2_ElRef[0].innerText = arrCube_5_ElRef[0].innerText = monthFrom;
+  arrCube_3_ElRef[0].innerText = arrCube_6_ElRef[0].innerText = yearFrom;
 
 
-  maxDays = getDaysfromMonthAndYear(month, year);
+  maxDays = getDaysfromMonthAndYear(monthFrom, yearFrom);
   
-  if(day == maxDays){
+  if(dayFrom == maxDays){
     arrCube_1_ElRef[1].innerText = 1;
     arrCube_1_ElRef[2].innerText = 2;
-    arrCube_1_ElRef[3].innerText = day - 1;
-  } else if(day == 1){
+    arrCube_1_ElRef[3].innerText = dayFrom - 1;
+  } else if(dayFrom == 1){
       if (month = 1) {month = 13;}
       maxDays = getDaysfromMonthAndYear(month - 1, year);
       arrCube_1_ElRef[1].innerText = 1;
       arrCube_1_ElRef[2].innerText = 2;
       arrCube_1_ElRef[3].innerText = maxDays;
   } else{
-    arrCube_1_ElRef[1].innerText = day + 1;
-    arrCube_1_ElRef[2].innerText = day + 2;
-    arrCube_1_ElRef[3].innerText = day - 1;
+    arrCube_1_ElRef[1].innerText = dayFrom + 1;
+    arrCube_1_ElRef[2].innerText = dayFrom + 2;
+    arrCube_1_ElRef[3].innerText = dayFrom - 1;
   }
 
 }
@@ -555,3 +568,240 @@ function isLeapYearCheck(varYear){
 
 
 /***********************************************************cube numbers end*******************************************************/
+
+/***********************************************************Calculation start*******************************************************/
+/*
+
+function getAllNumbers(){
+        
+  dayFrom = document.getElementsByClassName(arrCubeSidesClasses[arrSideCounter[0]])[0].innerText;
+  monthFrom = document.getElementsByClassName(arrCubeSidesClasses[arrSideCounter[1]])[0].innerText;
+  yearFrom = document.getElementsByClassName(arrCubeSidesClasses[arrSideCounter[2]])[0].innerText;
+  dayTo = document.getElementsByClassName(arrCubeSidesClasses[arrSideCounter[3]])[0].innerText;
+  monthTo = document.getElementsByClassName(arrCubeSidesClasses[arrSideCounter[4]])[0].innerText;
+  yearTo = document.getElementsByClassName(arrCubeSidesClasses[arrSideCounter[5]])[0].innerText;
+alert(arrCubeSidesClasses[arrSideCounter[0]]);
+  console.log('dayFrom: ' + dayFrom);
+  console.log('monthFrom: ' + monthFrom);
+  console.log('yearFrom: ' + yearFrom);
+  console.log('dayTo: ' + dayTo);
+  console.log('monthTo: ' + monthTo);
+  console.log('yearTo: ' + yearTo);
+}
+
+
+function calculateResult(){
+
+  getAllNumbers();
+
+  let dateFrom, dateTo;
+
+  let bReverseDate = bDateShouldBeReversed();           //just for test
+
+  if (bReverseDate != true && bReverseDate != false) {  //just for test
+    alert("Error in bDateShouldBeReversed() !!!!!!");
+  }
+
+  if (bReverseDate){
+    dateFrom = getDate("dateTo");
+    dateTo = getDate("dateFrom");
+
+  } else {
+    dateFrom = getDate("dateFrom");
+    dateTo = getDate("dateTo");
+  }
+
+
+  let arrDateFrom = dateFrom.split("-");
+  let arrDateTo = dateTo.split("-");
+
+  let dayFrom = arrDateFrom[0];
+  let monthFrom = arrDateFrom[1];
+  let yearFrom = arrDateFrom[2];
+
+  let dayTo = arrDateTo[0];
+  let monthTo = arrDateTo[1];
+  let yearTo = arrDateTo[2];
+
+  if (Number(yearFrom) > Number(yearTo)){
+  }
+
+
+  let daysQuantity = countDaysQuantity(Number(yearFrom), Number(yearTo), Number(monthFrom), Number(monthTo), Number(dayFrom), Number(dayTo));
+
+  let resultNumbers = document.getElementsByClassName("resultNumbers");
+  resultNumbers[0].innerText = daysQuantity; 
+
+
+}
+
+function countDaysQuantity(startYear, endYear, startMonth, endMonth, startDay, endDay){
+
+  let daysQuantity = 0;
+
+  if (endYear == startYear){
+
+
+    if (endMonth == startMonth) {
+
+
+      if (endDay == startDay){
+        daysQuantity = 0;
+
+      } else {
+        daysQuantity = endDay - startDay;
+    }
+
+
+    } else if (endMonth - startMonth > 1){
+
+      daysQuantity = daysQuantity + getDaysQuantityInMonth(startYear, startMonth) - startDay;
+      daysQuantity = daysQuantity + endDay;
+      daysQuantity = daysQuantity + getDaysOfSeveralMonths(startYear, startMonth + 1, endMonth);
+
+
+    } else if (endMonth - startMonth == 1){
+      daysQuantity = daysQuantity + getDaysQuantityInMonth(startYear, startMonth) - startDay;
+      daysQuantity = daysQuantity + endDay;
+
+
+    } else {
+      alert("Error in countDaysQuantity: 1.");
+
+    }
+    
+    
+  } else if(endYear - startYear > 1){
+
+    daysQuantity = daysQuantity + getDaysQuantityInMonth(startYear, startMonth) - startDay;
+    daysQuantity = daysQuantity + endDay;
+    daysQuantity = daysQuantity + getDaysOfSeveralMonths(startYear, startMonth + 1, 12 + 1);
+    daysQuantity = daysQuantity + getDaysOfSeveralMonths(endYear, 1, endMonth);
+    daysQuantity = daysQuantity + getDaysQuantityInYears(startYear + 1, endYear);
+
+
+  } else if(endYear - startYear == 1){
+    daysQuantity = daysQuantity + getDaysQuantityInMonth(startYear, startMonth) - startDay;
+    daysQuantity = daysQuantity + endDay;
+    daysQuantity = daysQuantity + getDaysOfSeveralMonths(startYear, startMonth + 1, 12 + 1);
+    daysQuantity = daysQuantity + getDaysOfSeveralMonths(endYear, 1, endMonth);
+
+
+  } else {
+    alert("Error in countDaysQuantity: 2.");
+  }
+
+
+  return daysQuantity;
+}
+
+function getDaysOfSeveralMonths(yearToCheck, firstMonthToCheck, secondMonthToCheck){
+
+  let daysNumber = 0;
+
+  while(firstMonthToCheck != secondMonthToCheck) {
+
+    daysNumber = daysNumber + getDaysQuantityInMonth(yearToCheck, firstMonthToCheck);
+
+    firstMonthToCheck = ++firstMonthToCheck;
+  }
+  
+  return daysNumber;
+}
+
+
+
+function getDaysQuantityInMonth(year, month){
+
+   switch(month){
+
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31;
+
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+  
+    case 2:
+      
+      if (year % 4 == 0){
+        return 29;
+      } else {
+        return 28;
+      }
+  }
+}
+
+function getDaysQuantityInYears(yearStart, yearEnd){
+
+  let daysNumber = 0;
+  
+  while(yearStart != yearEnd) {
+
+    if ( isLeapYearCheck(yearStart)){
+      daysNumber = daysNumber + 366;
+      yearStart = ++yearStart;
+
+    } else {
+    daysNumber = daysNumber + 365;
+    yearStart = ++yearStart;
+    }
+  }
+  return daysNumber;
+}
+
+function bDateShouldBeReversed(){
+
+  let yearFrom = document.getElementsByClassName("dateFrom")[0].children[1].children[2].innerText;
+  let yearTo = document.getElementsByClassName("dateTo")[0].children[1].children[2].innerText;
+
+  let monthFrom = document.getElementsByClassName("dateFrom")[0].children[1].children[1].innerText;
+  let monthTo = document.getElementsByClassName("dateTo")[0].children[1].children[1].innerText;
+
+  let dayFrom = document.getElementsByClassName("dateFrom")[0].children[1].children[0].innerText;
+  let dayTo = document.getElementsByClassName("dateTo")[0].children[1].children[0].innerText;
+
+  if(Number(yearFrom) - Number(yearTo) > 0){
+    return true;
+
+  } else if (yearFrom == yearTo) {
+
+    if (Number(monthFrom) - Number(monthTo) > 0){
+      return true;
+
+    } else if(monthFrom == monthTo){
+        if (Number(dayFrom) - Number(dayTo) > 0){
+          return true;
+
+        } else{
+          return false;
+        }
+            
+    } else{
+      return false;
+    }
+
+  } else {
+    return false;
+  }
+}
+
+function getDate(dateFromOrTo){
+  let date;
+  date = document.getElementsByClassName(dateFromOrTo)[0].children[1].children[0].innerText;
+  date = date + "-" + document.getElementsByClassName(dateFromOrTo)[0].children[1].children[1].innerText;
+  date = date + "-"+ document.getElementsByClassName(dateFromOrTo)[0].children[1].children[2].innerText;
+  return date;
+}
+
+
+
+/***********************************************************Calculation end*******************************************************/
