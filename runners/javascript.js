8266a342e4004685c1e8edec0b3dad33ea0efa50
GintarasRadzevicius <g.radzevicius@gmail.com>
2022-06-07 23:03:33 +0300

    Add files via upload

--- runners/javascript.js
+++ runners/javascript.js
@@ -5,63 +5,78 @@ let bstartButtonWasPressed = false;
 let finishLineCoord = document.getElementById('FinishLineSvg').getBoundingClientRect();
 let finishLineLeftCorner = finishLineCoord.left - 30;
 console.log('finishLineLeftCorner: ' + finishLineLeftCorner);
-let keepMoving = true;
-let winner;
+// let keepMoving = true;
+let winner = 0;
+let interval;
+let runnersRightCorner = 0;
 
 function startButton() {
     // resetRunners();
-    moveAllRunners();
+    activateResetButton();
+    interval = setInterval(moveAllRunners,5);
 
+    // showWinner();
+
+}
+
+function activateResetButton() {
+    document.getElementById('buttonStart').style.display = 'none';
+    document.getElementById('buttonReset').style.display = 'block';
+}
+
+function moveAllRunners() {
+    // while (keepMoving) {
+        for (let index = 1; index < 6; index++) {
+            moveRunner(index);
+            checkFinishLine(runnersRightCorner, index);
+            if (winner) {
+                clearInterval(interval);
+                console.log('winner: ' + winner);
+            }
+        }
+
+        // await sleep(2);
+    // }
+    // scale();
 }
 
 function showWinner() {
-switch (winner) {
-    case 1:
-        console.log('Winner is: rabbit')
-        break;
-    case 2:
-        console.log('Winner is: dog1')
+    console.log('winner: ' + winner);
+
+    switch (winner) {
+        case 1:
+            console.log('Winner is: rabbit')
+            break;
+        case 2:
+            console.log('Winner is: dog1')
+        
+            break;
+        case 3:
+            console.log('Winner is: cat')
     
-        break;
-    case 3:
-        console.log('Winner is: cat')
-
-        break;
-    case 4:
-        console.log('Winner is: horse')
-
-        break;
-    case 5:
-        console.log('Winner is: dog2')
-
-        break;
-                                        
-    default:
-        break;
-}
-
-}
-
-async function moveAllRunners() {
-    while (keepMoving) {
-        moveRunner(1);
-        moveRunner(2);
-        moveRunner(3);
-        moveRunner(4);
-        moveRunner(5);
-        await sleep(10);
+            break;
+        case 4:
+            console.log('Winner is: horse')
+    
+            break;
+        case 5:
+            console.log('Winner is: dog2')
+    
+            break;
+                                            
+        default:
+            break;
     }
-    showWinner();
-    scale();
+    
 }
 
 function scale() {
-    document.getElementById("runner" + winner).style.transition = "all 3s";
-    document.getElementById("runner" + winner).style.transform = "rotate(0)";
-    document.getElementById("runnerContainer" + winner).style.margin = "0 auto";
-    document.getElementById("runnerContainer" + winner).style.transform = "scale(5)";
-
-
+    document.getElementById("runner" + winner).style.margin = "0";
+    document.getElementById("runnerContainer" + winner).style.margin = "0";
+    document.getElementById("runner" + winner).style.position = 'absolute';
+    document.getElementById("runner" + winner).style.top = '50%';
+    document.getElementById("runner" + winner).style.left = '50%';
+    document.getElementById("runner" + winner).style.transform = 'translate(-50%, -50%)';
 }
 
 function moveRunner(runnerNo) {
@@ -71,20 +86,20 @@ function moveRunner(runnerNo) {
     let runnerPos = runnersMarginLeft[runnerNo-1] = runnersMarginLeft[runnerNo - 1] + randomInt;
     let runnerId = 'runner' + runnerNo;
     let runnerContainerId = 'runnerContainer' + runnerNo;
-    let runnersRightCorner = document.getElementById(runnerContainerId).offsetLeft + 80; //79 is runners width including borders 75+2+2
+    runnersRightCorner = document.getElementById(runnerContainerId).offsetLeft + 80; //79 is runners width including borders 75+2+2
 
     document.getElementById(runnerContainerId).style.marginLeft = runnerPos + 'px';
     document.getElementById(runnerId).style.transform = 'rotate(' + runnerPos + 'deg)';
-
-    checkFinishLine(runnersRightCorner, runnerNo);
 }
 
 function checkFinishLine(runnersRightCorner, runnerNo) {
     if (runnersRightCorner >= finishLineLeftCorner) {
         runnersRightCorner = finishLineLeftCorner ;
-        keepMoving = false;
+        // keepMoving = false;
         bstartButtonWasPressed = true;
-        winner = runnerNo;
+        if (!winner) {  //without it maybe because of setinteval there are several winners
+            winner = runnerNo;
+        }
     }
 }
 
@@ -96,18 +111,23 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function resetRunners() {
-    keepMoving = true;
-    runnersMarginLeft = [10, 10, 10, 10, 10];
+function resetButton() {
+    document.getElementById('buttonReset').style.display = 'none';
+    document.getElementById('buttonStart').style.display = 'block';
 
-    document.getElementById('runnerContainer1').style.marginLeft = '10 px';
-    document.getElementById('runner1').style.transform = 'rotate(0 deg)';
-    document.getElementById('runnerContainer2').style.marginLeft = '10 px';
-    document.getElementById('runner2').style.transform = 'rotate(0 deg)';
-    document.getElementById('runnerContainer3').style.marginLeft = '10 px';
-    document.getElementById('runner3').style.transform = 'rotate(0 deg)';
-    document.getElementById('runnerContainer4').style.marginLeft = '10 px';
-    document.getElementById('runner4').style.transform = 'rotate(0 deg)';
-    document.getElementById('runnerContainer5').style.marginLeft = '10 px';
-    document.getElementById('runner5').style.transform = 'rotate(0 deg)';
+    clearInterval(interval);
+
+    for (let index = 1; index < 6; index++) {
+        resetRunner(index);
+    }
+}
+
+function resetRunner(runnerNo) {
+    let runnerPos = runnersMarginLeft[runnerNo-1] = 10;
+    let runnerId = 'runner' + runnerNo;
+    let runnerContainerId = 'runnerContainer' + runnerNo;
+    runnersRightCorner = document.getElementById(runnerContainerId).offsetLeft + 80; //79 is runners width including borders 75+2+2
+
+    document.getElementById(runnerContainerId).style.marginLeft = runnerPos + 'px';
+    document.getElementById(runnerId).style.transform = 'rotate(0 deg)';
 }
